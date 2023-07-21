75cf834fa52eb8a99e13da425a929971203de813
fabiancrc124 <fabiancrc124@gmail.com>
2023-07-21 18:56:17 -0500

    Se agrega sub titulo

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,7 +6,9 @@ const Header = ({}) => {
     <>
       <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
          Seguimiento Pacientees {" "} 
-         <span className="text-indigo-600">veterinaria</span>
+         <span className="text-indigo-600">Veterinaria</span>
+         <br/>
+         <span className="text-indigo-600">Gestion de citas</span>
       </h1>
      
     </>
